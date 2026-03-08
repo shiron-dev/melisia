8ff977d04f150fa431ab79a7ba64b7aed595569d
shiron <shiron4710.dev@gmail.com>
2026-03-08 22:21:14 +0900

    refactor: simplify Google Cloud project service definitions in Terraform

--- terraform/terraform/api.tf
+++ terraform/terraform/api.tf
@@ -1,32 +1,11 @@
-resource "google_project_service" "cloudresourcemanager" {
-  service            = "cloudresourcemanager.googleapis.com"
-  disable_on_destroy = false
-}
-
-resource "google_project_service" "iam" {
-  service            = "iam.googleapis.com"
-  disable_on_destroy = false
-
-  depends_on = [google_project_service.cloudresourcemanager]
-}
-
 resource "google_project_service" "cloudkms" {
-  service            = "cloudkms.googleapis.com"
-  disable_on_destroy = false
-
-  depends_on = [google_project_service.cloudresourcemanager]
+  service = "cloudkms.googleapis.com"
 }
 
 resource "google_project_service" "iamcredentials" {
-  service            = "iamcredentials.googleapis.com"
-  disable_on_destroy = false
-
-  depends_on = [google_project_service.cloudresourcemanager]
+  service = "iamcredentials.googleapis.com"
 }
 
 resource "google_project_service" "sts" {
-  service            = "sts.googleapis.com"
-  disable_on_destroy = false
-
-  depends_on = [google_project_service.cloudresourcemanager]
+  service = "sts.googleapis.com"
 }
