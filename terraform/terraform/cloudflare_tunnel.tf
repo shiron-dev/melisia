53e81efc358bee0b9d4d273b2d6e6c93d2a640ec
Itsuki Shirahige <shiron4710.dev@gmail.com>
2026-05-24 01:10:01 +0900

    feat(cloudflare): add home-ep mesh access

--- terraform/terraform/cloudflare_tunnel.tf
+++ terraform/terraform/cloudflare_tunnel.tf
@@ -18,16 +18,73 @@ locals {
   cloudflare_tunnels = {
     "arm-srv-snipeit" = {
       domain          = "snipeit.shiron.dev"
+      zone_name       = "shiron.dev"
       service         = "http://snipeit-app:80"
-      secret_yaml_dir = "${path.module}/../compose/hosts/arm-srv/snipeit"
+      secret_yaml_dir = "${path.module}/../../compose/hosts/arm-srv/snipeit"
       policies        = local.cloudflare_access_policy_refs.shiron
     }
+    "home-ep-homeassistant" = {
+      domain          = "home.melisia.net"
+      zone_name       = "melisia.net"
+      service         = "http://homeassistant:8123"
+      secret_yaml_dir = "${path.module}/../../compose/hosts/home-ep/home-assistant"
+      policies        = []
+      extra_ingress = [
+        {
+          hostname  = "zigbee2mqtt.melisia.net"
+          zone_name = "melisia.net"
+          service   = "http://zigbee2mqtt:8080"
+          policies  = local.cloudflare_access_policy_refs.shiron
+        },
+        {
+          hostname  = "switchbot-mqtt.melisia.net"
+          zone_name = "melisia.net"
+          service   = "http://switchbot-mqtt:8099"
+          policies  = local.cloudflare_access_policy_refs.shiron
+        },
+        {
+          hostname  = "esphome.melisia.net"
+          zone_name = "melisia.net"
+          service   = "http://esphome:6052"
+          policies  = local.cloudflare_access_policy_refs.shiron
+        }
+      ]
+    }
+  }
+}
+
+locals {
+  extra_tunnel_ingress_map = {
+    for item in flatten([
+      for tunnel_key, tunnel in local.cloudflare_tunnels : [
+        for ingress in lookup(tunnel, "extra_ingress", []) : {
+          tunnel_key = tunnel_key
+          hostname   = ingress.hostname
+          zone_name  = ingress.zone_name
+          service    = ingress.service
+          policies   = lookup(ingress, "policies", [])
+        }
+      ]
+    ]) : "${item.tunnel_key}-${item.hostname}" => item
+  }
+}
+
+locals {
+  cloudflare_zone_ids = {
+    "shiron.dev"  = data.cloudflare_zone.this.zone_id
+    "melisia.net" = data.cloudflare_zone.melisia_net.zone_id
   }
 }
 
 data "cloudflare_zone" "this" {
   filter = {
     name = local.cloudflare_zone_name
+  }
+}
+
+data "cloudflare_zone" "melisia_net" {
+  filter = {
+    name = "melisia.net"
   }
 }
 
@@ -54,23 +111,31 @@ resource "cloudflare_zero_trust_tunnel_cloudflared_config" "this" {
   source     = "cloudflare"
 
   config = {
-    ingress = [
-      {
-        hostname = each.value.domain
-        service  = each.value.service
-      },
-      {
-        service = "http_status:404"
-      }
-    ]
+    ingress = concat(
+      [
+        {
+          hostname = each.value.domain
+          service  = each.value.service
+        }
+      ],
+      [for i in lookup(each.value, "extra_ingress", []) : {
+        hostname = i.hostname
+        service  = i.service
+      }],
+      [
+        {
+          service = "http_status:404"
+        }
+      ]
+    )
   }
 }
 
 resource "cloudflare_dns_record" "tunnel" {
   for_each = local.cloudflare_tunnels
 
-  zone_id = data.cloudflare_zone.this.zone_id
-  name    = trimsuffix(each.value.domain, ".${local.cloudflare_zone_name}")
+  zone_id = local.cloudflare_zone_ids[each.value.zone_name]
+  name    = trimsuffix(each.value.domain, ".${each.value.zone_name}")
   type    = "CNAME"
   content = "${cloudflare_zero_trust_tunnel_cloudflared.this[each.key].id}.cfargotunnel.com"
   ttl     = 1
@@ -84,8 +149,32 @@ data "cloudflare_zero_trust_tunnel_cloudflared_token" "this" {
   tunnel_id  = cloudflare_zero_trust_tunnel_cloudflared.this[each.key].id
 }
 
+resource "cloudflare_dns_record" "extra_tunnel_ingress" {
+  for_each = local.extra_tunnel_ingress_map
+
+  zone_id = local.cloudflare_zone_ids[each.value.zone_name]
+  name    = trimsuffix(each.value.hostname, ".${each.value.zone_name}")
+  type    = "CNAME"
+  content = "${cloudflare_zero_trust_tunnel_cloudflared.this[each.value.tunnel_key].id}.cfargotunnel.com"
+  ttl     = 1
+  proxied = true
+}
+
+resource "cloudflare_zero_trust_access_application" "extra_tunnel_ingress" {
+  for_each = { for k, v in local.extra_tunnel_ingress_map : k => v if length(v.policies) > 0 }
+
+  account_id                = local.cloudflare_account_id
+  name                      = "${each.key}${local.cloudflare_resource_name_suffix}"
+  domain                    = each.value.hostname
+  type                      = "self_hosted"
+  session_duration          = "24h"
+  service_auth_401_redirect = false
+
+  policies = each.value.policies
+}
+
 resource "cloudflare_zero_trust_access_application" "this" {
-  for_each = local.cloudflare_tunnels
+  for_each = { for k, v in local.cloudflare_tunnels : k => v if length(v.policies) > 0 }
 
   account_id                = local.cloudflare_account_id
   name                      = "${each.key}${local.cloudflare_resource_name_suffix}"
@@ -95,6 +184,30 @@ resource "cloudflare_zero_trust_access_application" "this" {
   service_auth_401_redirect = false
 
   policies = each.value.policies
+}
+
+resource "cloudflare_zero_trust_access_application" "home_ep_homeassistant" {
+  account_id                = local.cloudflare_account_id
+  name                      = "home-ep-homeassistant${local.cloudflare_resource_name_suffix}"
+  domain                    = "home.melisia.net/auth/authorize"
+  type                      = "self_hosted"
+  session_duration          = "24h"
+  service_auth_401_redirect = false
+
+  policies = [
+    {
+      name       = "home login"
+      decision   = "allow"
+      precedence = 1
+      include = [
+        {
+          group = {
+            id = "09b05356-05d0-4f9e-89db-b163531b01dc"
+          }
+        }
+      ]
+    }
+  ]
 }
 
 resource "local_sensitive_file" "cloudflare_tunnel_secret" {
--- terraform/terraform/variables.tf
+++ terraform/terraform/variables.tf
@@ -1,5 +1,16 @@
 variable "cloudflare_api_token" {
-  description = "Cloudflare API Token"
+  description = "Cloudflare API Token. Mesh requires Account: Cloudflare One Connectors Write (or Cloudflare One Connector: WARP Write), Cloudflare One Networks Write / Cloudflare Tunnel Write for routes, and Zero Trust Write for WARP device profile split tunnels."
   type        = string
   sensitive   = true
+}
+
+variable "home_ep_mesh_ip" {
+  description = "Cloudflare Mesh IP assigned to home-ep, for example 100.96.x.y. Leave null until the node is enrolled."
+  type        = string
+  default     = null
+
+  validation {
+    condition     = var.home_ep_mesh_ip == null || can(cidrhost("${var.home_ep_mesh_ip}/32", 0))
+    error_message = "home_ep_mesh_ip must be a valid IPv4 address."
+  }
 }
