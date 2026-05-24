29b425f555fb9911c3143d6cd5b6e497bfe49bc2
shiron <shiron4710.dev@gmail.com>
2026-05-24 12:13:09 +0900

    ci(terraform): use static cleanup wif

--- terraform/github_secrets/github_actions_wif_secrets.tf
+++ terraform/github_secrets/github_actions_wif_secrets.tf
@@ -15,15 +15,3 @@ resource "github_actions_environment_secret" "service_account" {
   secret_name     = "SERVICE_ACCOUNT"
   plaintext_value = data.terraform_remote_state.main.outputs.service_account
 }
-
-resource "github_actions_secret" "cleanup_workload_identity_provider" {
-  repository      = local.github_repository
-  secret_name     = "CLEANUP_WORKLOAD_IDENTITY_PROVIDER"
-  plaintext_value = data.terraform_remote_state.main.outputs.workload_identity_provider
-}
-
-resource "github_actions_secret" "cleanup_service_account" {
-  repository      = local.github_repository
-  secret_name     = "CLEANUP_SERVICE_ACCOUNT"
-  plaintext_value = data.terraform_remote_state.main.outputs.service_account
-}
