b1258a6406c83a9307c4c17a9cc654d5111d5b2c
shiron <shiron4710.dev@gmail.com>
2026-06-07 03:29:15 +0900

    refactor: split Cloudflare Terraform files

---
+++ terraform/terraform/cloudflare_tunnel_dns.tf
@@ -0,0 +1,21 @@
+resource "cloudflare_dns_record" "tunnel" {
+  for_each = local.cloudflare_tunnels
+
+  zone_id = local.cloudflare_zone_ids[each.value.zone_name]
+  name    = trimsuffix(each.value.domain, ".${each.value.zone_name}")
+  type    = "CNAME"
+  content = "${cloudflare_zero_trust_tunnel_cloudflared.this[each.key].id}.cfargotunnel.com"
+  ttl     = 1
+  proxied = true
+}
+
+resource "cloudflare_dns_record" "extra_tunnel_ingress" {
+  for_each = local.extra_tunnel_ingress_map
+
+  zone_id = local.cloudflare_zone_ids[each.value.zone_name]
+  name    = trimsuffix(each.value.hostname, ".${each.value.zone_name}")
+  type    = "CNAME"
+  content = "${cloudflare_zero_trust_tunnel_cloudflared.this[each.value.tunnel_key].id}.cfargotunnel.com"
+  ttl     = 1
+  proxied = true
+}
